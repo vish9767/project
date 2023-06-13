ef576f9a24b663a38cb3bb16dc56f73cded82820
SandipShimpi001 <129252301+SandipShimpi001@users.noreply.github.com>
2023-06-13 15:58:49 +0530

    comment unused import files from hhc app models

--- hhcapp/models.py
+++ hhcapp/models.py
@@ -1,15 +1,15 @@
-from django.db import models
-from hhcweb.models import active_inactive_enum
-from django_enumfield import enum
-from hhcweb import models as webmodels
+# from django.db import models
+# from hhcweb.models import active_inactive_enum
+# from django_enumfield import enum
+# from hhcweb import models as webmodels
 
-# Create your models here.
+# # Create your models here.
 
 
-class status_enum(enum.Enum):
-	Active = 1
-	Inactive = 2
-	Delete = 3
+# class status_enum(enum.Enum):
+# 	Active = 1
+# 	Inactive = 2
+# 	Delete = 3
 """
 
 class agg_hhc_app_caller_register(models.Model):#113
