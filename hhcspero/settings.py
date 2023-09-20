ba9c95b358fb981f8cdb68dfa776599aff108777
Vishala97 <127483728+vishala97@users.noreply.github.com>
2023-09-20 19:21:46 +0530

    done

--- hhcspero/settings.py
+++ hhcspero/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE' : 'django.db.backends.postgresql',
-        'NAME' : 'hhc_database',#hhc_database#test5
+        'NAME' : 'test5',#hhc_database#test5
         'USER' : 'postgres',
         'PASSWORD' : 'root',
         'HOST' : 'localhost',
