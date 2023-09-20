642ec87cb2a954e907f482fabb4e4a4dd565893d
roshnik12 <74372627+roshnik12@users.noreply.github.com>
2023-09-20 17:50:37 +0530

    database changed

--- hhcspero/settings.py
+++ hhcspero/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE' : 'django.db.backends.postgresql',
-        'NAME' : 'test5',
+        'NAME' : 'test5',#hhc_database
         'USER' : 'postgres',
         'PASSWORD' : 'root',
         'HOST' : 'localhost',
